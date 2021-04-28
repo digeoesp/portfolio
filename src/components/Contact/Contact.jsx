8ca1bdd3d859755995d17ad797a05a433a034f64
digeoesp <diegocrypto@outlook.com>
2021-04-28 12:33:35 -0700

    added email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -17,6 +17,7 @@ const Contact = () => {
             <p className="contact-wrapper__text">
               {cta || 'Would you like to work with me? Awesome!'}
             </p>
+            <p className="contact-wrapper__text">diegoespinoza475@outlook.com</p>
             <a
               target="_blank"
               rel="noopener noreferrer"
