960fba6222e3b92de06e262431362cfae7350eb5
digeoesp <diegocrypto@outlook.com>
2021-05-03 21:01:27 -0700

    made css responsive

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -35,27 +35,57 @@ const Skill = () => {
         <SiGithub id="git-icon" />
         <SiAdobephotoshop id="ps-icon" />
         <SiAdobelightroomcc id="lr-icon" />
-        <SiJson id="icon" />
+        <SiJson id="js-icon" />
         <SiPostman id="pm-icon" />
-        <SiJupyter id="icon" />
+        <SiJupyter id="jp-icon" />
       </div>
 
       <div className="icon-desc">
-        <p className="javascript">JavaScript</p>
-        <p className="html">Html</p>
-        <p className="css">Css</p>
-        <p className="javascript">Node.Js</p>
-        <p className="javascript">PostgresQl</p>
-        <p className="javascript">React</p>
-        <p className="javascript">MongoDb</p>
-        <p className="javascript">Python</p>
-        <p className="javascript">Django</p>
-        <p className="javascript">GitHub</p>
-        <p className="javascript">PhotoShop</p>
-        <p className="javascript">LightRoom</p>
-        <p className="javascript">JSON</p>
-        <p className="javascript">Postman</p>
-        <p className="jupyter">Jupyter</p>
+        <p className="javascript" id="java-text">
+          JavaScript
+        </p>
+        <p className="html" id="html-text">
+          Html
+        </p>
+        <p className="css" id="css-text">
+          CSS
+        </p>
+        <p className="javascript" id="node-text">
+          Node.Js
+        </p>
+        <p className="javascript" id="pstg-text">
+          PostgresQl
+        </p>
+        <p className="javascript" id="react-text">
+          React
+        </p>
+        <p className="javascript" id="mongo-text">
+          MongoDb
+        </p>
+        <p className="javascript" id="python-text">
+          Python
+        </p>
+        <p className="javascript" id="django-text">
+          Django
+        </p>
+        <p className="javascript" id="git-text">
+          GitHub
+        </p>
+        <p className="javascript" id="ps-text">
+          PhotoShop
+        </p>
+        <p className="javascript" id="lr-text">
+          LightRoom
+        </p>
+        <p className="javascript" id="json-text">
+          JSON
+        </p>
+        <p className="javascript" id="pm-text">
+          Postman
+        </p>
+        <p className="jupyter" id="jupy-text">
+          Jupyter
+        </p>
       </div>
       <h3 className="skills-footer">And many more</h3>
     </div>
